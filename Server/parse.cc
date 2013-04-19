d529aa5381e28db06898f47eb056ead1cc41c519
Bill Ford <bill.ford@utah.edu>
2013-04-18 18:55:41 -0600

    updated parse.h parse.cc with namespace

--- Server/parse.cc
+++ Server/parse.cc
@@ -4,27 +4,29 @@
 
 using namespace std;
 
-// gets the indexed word from the line for parsing the data 
-std::string get_word(int word_index, std::string str, char ch) {
-  int sp_index = 1;
-  std::string word;  
+namespace serverss {
 
-  for (int i = 0; i < str.length(); i++) {
+  // gets the indexed word from the line for parsing the data 
+  std::string get_word(int word_index, std::string str, char ch) {
+    int sp_index = 1;
+    std::string word;  
+
+    for (int i = 0; i < str.length(); i++) {
   
-  if (sp_index > word_index) { 
-    return word; 
-  } // if
-
-  if (str[i] == ch || str[i] == '\r') { 
-    sp_index++;
-  } // if
-  else if (sp_index == word_index) {
-    word.push_back(str[i]);
-  } // else if
-
-  } // for
-  return word;
+    if (sp_index > word_index) { 
+      return word; 
+    } // if
+  
+    if (str[i] == ch || str[i] == '\r') { 
+      sp_index++;
+    } // if
+    else if (sp_index == word_index) {
+      word.push_back(str[i]);
+    } // else if
+  
+    } // for
+    return word;
+  }
 }
-
 
 
--- Server/parse.h
+++ Server/parse.h
@@ -1,7 +1,8 @@
 #include <iostream>
 
+namespace serverss {
 
-// gets the indexed word from the line for parsing the data
-std::string get_word(int word_index, std::string str, char ch);
+  // gets the indexed word from the line for parsing the data
+  std::string get_word(int word_index, std::string str, char ch);
 
- 
+} 

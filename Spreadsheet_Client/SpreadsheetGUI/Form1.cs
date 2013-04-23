4057c69b57a310680c6a9e1827caabe1439bd6da
Andres <andresf.mon@gmail.com>
2013-04-23 12:40:18 -0600

    Revert "fixed close"
    This reverts commit a7a1ec5057a84b4a2e5332ac753085105ff7f3d3.

--- Spreadsheet_Client/SpreadsheetGUI/Form1.cs
+++ Spreadsheet_Client/SpreadsheetGUI/Form1.cs
@@ -121,7 +121,7 @@ namespace SS
         private void closeToolStripMenuItem_Click(object sender, EventArgs e)
         {
             myModel.Leave(myName);
-            //this.Close();
+            this.Close();
             Application.Exit();
         }
         
@@ -650,8 +650,7 @@ namespace SS
 
         private void Form1_FormClosed(object sender, FormClosedEventArgs e)
         {
-            myModel.Leave(myName);
-            Application.Exit();
+            closeToolStripMenuItem_Click(sender, e);
             
         }
         private void OpenNew(Spreadsheet openSheet, string filename)

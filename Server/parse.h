f45e1a42467eac7605618482c585b210a0f7c279
Bill Ford <bill.ford@utah.edu>
2013-04-22 18:04:08 -0600

    fixed put_xml with header

--- Server/parse.h
+++ Server/parse.h
@@ -8,7 +8,7 @@ namespace serverss {
 
 
   // header for all spreadsheets 
-  const std::string  header = "<?xml version=\"1.0\" encoding=\"utf-8\"?>";
+  const std::string  header = "<?xml version=\"1.0\" encoding=\"utf-8\"?><spreadsheet>";
   //const std::string  header = "<?xml version=\"1.0\" encoding=\"utf-8\"?><spreadsheet version=\"ps6\"/>";
   const std::string ssdb = "data/ss.db";
 
